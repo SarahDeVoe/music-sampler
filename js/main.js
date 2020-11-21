39a3105afbe7986aa85780df084328824e294195
Sarah Devoe <sarahdevoe@Sarahs-MacBook-Pro-2.local>
2020-11-20 23:40:17 -0500

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let album1 = document.querySelector('youngandhopeless')
+let album2 = document.querySelector('cardiology')
+let album3 = document.querySelector('youthauthority')
+let album4 = document.querySelector('generationrx')
+let song1 = document.querySelector('anthem')
+let song2 = document.querySelector('birthday')
+let song3 = document.querySelector('changes')
+let song4 = document.querySelector('shadow')
+
+album1.onclick = () => 
